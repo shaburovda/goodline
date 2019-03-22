bece68d4fab2abefe8ff47018ff73aa57518e51f
dmitry <dmitry.shaburov@yandex.ru>
2019-03-22 06:04:28 +0000

    Tru push

--- src/com/dmitry/aaa/Main.kt
+++ src/com/dmitry/aaa/Main.kt
@@ -2,7 +2,7 @@ package com.dmitry.aaa
 
 fun main(args: Array<String>) {
     if (args.isEmpty()) {
-        println("No Args!!!")
+        println("No Args!!!!!")
     } else {
         args.forEach { println(it) }
     }

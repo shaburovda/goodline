5d67107545437d85319e7c7d3eed2c4401063dc9
dmitry <dmitry.shaburov@yandex.ru>
2019-03-23 05:34:55 +0000

    Add defaul callback value

--- src/com/dmitry/aaa/Main.kt
+++ src/com/dmitry/aaa/Main.kt
@@ -3,16 +3,20 @@ package com.dmitry.aaa
 fun main(args: Array<String>) {
 
     val status = when (args.size) {
-        0 -> getStatus(0, { println("No args!!!") })
-        1 -> getStatus(1, {})
-        2 -> getStatus(2, {})
-        else -> getStatus(100, { args.forEach { println(it) } })
+        0 -> getStatus(0) {
+            println("No args!!!")
+        }
+        1 -> getStatus(1)
+        2 -> getStatus(2)
+        else -> getStatus(100) {
+            args.forEach { println(it) }
+        }
     }
 
     System.exit(status)
 }
 
-fun getStatus(status: Int, callback: () -> Unit): Int {
+fun getStatus(status: Int, callback: () -> Unit = {}): Int {
     callback()
     return status
 }
